5334b8999d6710251d80d44ed452a0d10ff8c55d
Claudio Marques <admin@hostingbr.cloud>
2023-06-23 02:23:42 -0300

    cache enabled

--- controllers/fipe.js
+++ controllers/fipe.js
@@ -5,8 +5,9 @@ const DB = require('../components/db');
 const URL_BASE = "https://veiculos.fipe.org.br/api/veiculos/";
 const dataTable = process.env.FIPE_TABLE || 295; // Março/2023;
 const dataTableUpdate = new Date("2023-03");
-const cacheEnabled = Boolean(process.env.CACHE_ENABLED === "true") || false;
-const DEBUG = Boolean(process.env.DEBUG === "true" || false) || false;
+const cacheEnabled = true;
+const DEBUG = true;
+
 
 // Get types
 function getTypes(vehicleType) {
